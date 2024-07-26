24f8b31d94ce77f80eaaa76adbcb5474b66af7ab
anton1945 <james.anton1945@gmail.com>
2024-07-26 16:26:36 +0300

    third commit

--- spenn_api/settings.py
+++ spenn_api/settings.py
@@ -100,9 +100,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'spenn_api',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'aDnLs4gb9w',
         'HOST': 'localhost',
-        'PORT': '5433',
+        'PORT': '5432',
     }
 }
 
